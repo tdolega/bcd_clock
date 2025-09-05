650e02bbc88075c790d402d065317c1c702c4454
tdolega <tymoteuszdolega@gmail.com>
2025-09-05 22:11:36 +0200

    update code for new esp32 lib

--- bcd_clock.ino
+++ bcd_clock.ino
@@ -34,8 +34,8 @@ const char* NTP_SERVER    = "pool.ntp.org";
 
 //// 50/50 CONFIG
 
-const int PWM_FREQUENCY          = 10 kHz;
-const int PWM_RESOLUTION         = 8;
+const uint32_t PWM_FREQUENCY     = 10 kHz;
+const uint8_t  PWM_RESOLUTION    = 12; // 1-20 [bits]
 
 const int THERMOMETER_RESOLUTION = 12; // 9-12 [bits]
 
@@ -82,30 +82,10 @@ const int digit_to_BCD[10][LEDS_ROWS] = {
 const int GPIO_PIN_THERMOMETER = 32;
 const int GPIO_PIN_BUTTON      = 35;
 
-const int PWM_CHANNEL_LEDS_OFF = 0;
-const int PWM_CHANNEL_LEDS_ON  = 1;
+const int8_t PWM_CHANNEL_LEDS_OFF = 0;
+const int8_t PWM_CHANNEL_LEDS_ON  = 1;
 
 const int THERMOMETER_ONE_WIRE_IDX = 0;
-
-//// ENUMS
-
-typedef enum Brightness {
-  B_FULL = 255,
-  B_HIGH = 80,
-  B_MEDIUM = 20,
-  B_LOW = 1,
-} Brightness;
-
-typedef enum Modes {
-  M_CLOCK,
-  M_THERMOMETER,
-  M_BLANK,
-} Modes;
-
-//// STARTUP CONFIG
-
-Brightness brightness = B_LOW;
-Modes mode = M_CLOCK;
 
 //// VARIABLES
 
@@ -113,14 +93,60 @@ int LEDS[LEDS_COLS][LEDS_ROWS]; // LEDs states
 
 int button_pressed_ts = 0; // 0: button is not pressed, >0: ts of button press start
 
-float temperature_reading_celsius = -1024.0;
-int temperature_reading_celsius_floor = 0;
-int temperature_request_ts = 0;
+volatile float temperature_reading_celsius = -1024.0;
+volatile int temperature_reading_celsius_abs_int = 0;
+volatile int temperature_request_ts = 0;
 
 int last_displayed_seconds = -1024;
 int last_displayed_temperature = -1024;
 
 TaskHandle_t background_thread_handle = NULL;
+
+//// BRIGHTNESS
+
+typedef enum Brightness {
+  // depends on PWM_RESOLUTION
+  B_FULL = (1 << PWM_RESOLUTION) - 1, // 4095 for 12 bits
+  B_HIGH = 128,
+  B_MEDIUM = 8,
+  B_LOW = 2,
+} Brightness;
+
+Brightness brightness = B_MEDIUM;
+
+void change_to_next_brightness_level() {
+  switch(brightness) {
+    case B_FULL:       brightness = B_HIGH;    break;
+    case B_HIGH:       brightness = B_MEDIUM;  break;
+    case B_MEDIUM:     brightness = B_LOW;     break;
+    case B_LOW:        brightness = B_FULL;    break;
+  }
+  ledcWriteChannel(PWM_CHANNEL_LEDS_ON, brightness);
+}
+
+//// MODES
+
+typedef enum Modes {
+  M_CLOCK,
+  M_THERMOMETER,
+  M_BLANK,
+} Modes;
+
+Modes mode = M_CLOCK;
+
+void change_to_next_mode() {
+  switch(mode) {
+    case M_CLOCK:        mode = M_THERMOMETER;     break;
+    case M_THERMOMETER:  mode = M_BLANK;           break;
+    case M_BLANK:        mode = M_CLOCK;           break;
+  }
+  // force update on next loop
+  last_displayed_seconds = -1024;
+  last_displayed_temperature = -1024;
+  // clear screen
+  set_all(LOW);
+  apply_leds();
+}
 
 //// OBJECTS
 
@@ -133,34 +159,10 @@ AsyncMqttClient mqtt_client;
 
 //// FUNCTIONS
 
-void change_to_next_brightness_level() {
-  switch(brightness) {
-    case B_FULL:       brightness = B_HIGH;    break;
-    case B_HIGH:       brightness = B_MEDIUM;  break;
-    case B_MEDIUM:     brightness = B_LOW;     break;
-    case B_LOW:        brightness = B_FULL;    break;
-  }
-  ledcWrite(PWM_CHANNEL_LEDS_ON, brightness);
-}
-
-void change_to_next_mode() {
-  switch(mode) {
-    case M_CLOCK:        mode = M_THERMOMETER;     break;
-    case M_THERMOMETER:  mode = M_BLANK;           break;
-    case M_BLANK:        mode = M_CLOCK;           break;
-  }
-  // force update on next loop
-  last_displayed_seconds = -1024;
-  last_displayed_temperature = -1024;
-  // clear screen
-  set_all_leds(LOW);
-}
-
-void set_all_leds(int state) {
+void set_all(int state) {
   for(int col = 0; col < LEDS_COLS; col++)
     for(int row = 0; row < LEDS_ROWS; row++)
       LEDS[col][row] = state;
-  set_leds();
 }
 
 void set_digit(int col, int digit) {
@@ -174,14 +176,17 @@ void set_number(int group, int number) {
   set_digit(group * 2 + 1, number % 10);
 }
 
-void set_leds() {
-  for(int col = 0; col < LEDS_COLS; col++){
+void apply_leds() {
+  for(int col = 0; col < LEDS_COLS; col++) {
     for(int row = 0; row < LEDS_ROWS; row++) {
       if(GPIO_DIGITS[col][row] == NO) continue;
       int channel = LEDS[col][row] ? PWM_CHANNEL_LEDS_ON : PWM_CHANNEL_LEDS_OFF;
-      ledcAttachPin(GPIO_DIGITS[col][row], channel);
+      ledcDetach(GPIO_DIGITS[col][row]);
+      ledcAttachChannel(GPIO_DIGITS[col][row], PWM_FREQUENCY, PWM_RESOLUTION, channel);
     }
   }
+  ledcWriteChannel(PWM_CHANNEL_LEDS_OFF, 0);
+  ledcWriteChannel(PWM_CHANNEL_LEDS_ON, brightness);
 }
 
 void update_temperature() {
@@ -193,7 +198,7 @@ void update_temperature_force() {
   temperature_request_ts = millis();
   thermometer.requestTemperatures();
   temperature_reading_celsius = thermometer.getTempCByIndex(THERMOMETER_ONE_WIRE_IDX);
-  temperature_reading_celsius_floor = floor(temperature_reading_celsius);
+  temperature_reading_celsius_abs_int = abs(int(temperature_reading_celsius));
 }
 
 //// RUNTIME
@@ -202,14 +207,10 @@ void display_clock() {
   struct tm timeinfo;
 
   if (!getLocalTime(&timeinfo)) {
+    set_all(LOW);
     LEDS[0][0] = HIGH;
     LEDS[0][1] = WiFi.isConnected() ? HIGH : LOW;
-    set_leds();
-    
-    // setup time
-    configTime(0, 0, NTP_SERVER);
-    tzset();
-    
+    apply_leds();    
     return;
   }
 
@@ -220,28 +221,33 @@ void display_clock() {
   set_number(0, timeinfo.tm_hour);
   set_number(1, timeinfo.tm_min);
   set_number(2, timeinfo.tm_sec);
-  set_leds();
+  apply_leds();
 }
 
 void display_temperature() {
   update_temperature();
 
   // update screen only when needed
-  if (last_displayed_temperature == temperature_reading_celsius_floor) return;
+  if (last_displayed_temperature == temperature_reading_celsius_abs_int) return;
+
+  if (temperature_reading_celsius_abs_int < -100 || temperature_reading_celsius_abs_int > 100) {
+    temperature_reading_celsius_abs_int = 77;
+  }
 
   bool is_negative = temperature_reading_celsius < 0;
   LEDS[0][1] = LEDS[1][1] = is_negative ? HIGH : LOW; // minus sign
-  set_number(1, temperature_reading_celsius_floor);
-  set_leds();
+  set_number(1, temperature_reading_celsius_abs_int);
+  apply_leds();
 
-  last_displayed_temperature = temperature_reading_celsius_floor;
+  last_displayed_temperature = temperature_reading_celsius_abs_int;
 }
 
 void background_thread(void* pvParameters) {
   for ever {
     update_temperature();
     delay(35 s);
-    mqtt_client.publish(MQTT_TOPIC, 1, true, String(temperature_reading_celsius, 2).c_str());
+    String payload = String(temperature_reading_celsius, 2);
+    mqtt_client.publish(MQTT_TOPIC, 1, true, payload.c_str());
     delay(35 s);
   }
 }
@@ -267,17 +273,13 @@ void e_mqtt_disconnected(AsyncMqttClientDisconnectReason reason) {
 }
 
 void setup() {
-  // setup LEDs channels
-  ledcSetup(PWM_CHANNEL_LEDS_OFF, PWM_FREQUENCY, PWM_RESOLUTION);
-  ledcSetup(PWM_CHANNEL_LEDS_ON , PWM_FREQUENCY, PWM_RESOLUTION);
-  // setup channels brightness
-  ledcWrite(PWM_CHANNEL_LEDS_OFF, 0);
-  ledcWrite(PWM_CHANNEL_LEDS_ON , brightness);
-
-  set_all_leds(LOW);
-
   // setup button
   button.setDebounceTime(BUTTON_DEBOUNCE_MS);
+
+  // setup timezone
+  configTime(0, 0, NTP_SERVER);
+  setenv("TZ", TIMEZONE_ENV, 1);
+  tzset();
 
   // setup Wi-Fi
   WiFi.onEvent(e_wifi_connected   , ARDUINO_EVENT_WIFI_STA_GOT_IP      );
@@ -285,13 +287,18 @@ void setup() {
   WiFi.setHostname(HOSTNAME);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
+  // signalize Wi-Fi setup complete
+  set_digit(1, 0b1);
+  apply_leds();
+
   // setup MQTT
   mqtt_client.onDisconnect(e_mqtt_disconnected);
   mqtt_client.setServer(MQTT_HOST, MQTT_PORT);
   mqtt_client.setCredentials(MQTT_USER, MQTT_PASSWORD);
 
-  // timezone env var
-  setenv("TZ", TIMEZONE_ENV, 1);
+  // signalize MQTT setup complete
+  set_digit(1, 0b11);
+  apply_leds();
   
   // setup thermometer
   thermometer.begin();
@@ -337,6 +344,7 @@ void loop() {
   switch(mode) {
     case M_CLOCK       : display_clock()       ;break;
     case M_THERMOMETER : display_temperature() ;break;
+    case M_BLANK       :                       ;break;
   }
 
   // chill
