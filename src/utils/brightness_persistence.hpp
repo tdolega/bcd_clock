51d02774dd7b3d6567b4d0364ef25159e04ebecf
tdolega <tymoteuszdolega@gmail.com>
2026-05-29 20:25:39 +0200

    Improve brightness persistence by ensuring value is only updated if changed

--- src/utils/brightness_persistence.hpp
+++ src/utils/brightness_persistence.hpp
@@ -7,13 +7,16 @@ Preferences brightness_prefs;
 
 inline void save_brightness(uint16_t brightness) {
   brightness_prefs.begin("bcd_clock", false);
-  brightness_prefs.putUInt("brightness", brightness);
+  uint16_t existing = brightness_prefs.getUShort("brightness", 0xFFFF);
+  if (existing != brightness) {
+    brightness_prefs.putUShort("brightness", brightness);
+  }
   brightness_prefs.end();
 }
 
 inline uint16_t load_brightness() {
   brightness_prefs.begin("bcd_clock", true);
-  uint16_t brightness = brightness_prefs.getUInt("brightness", 128);
+  uint16_t brightness = brightness_prefs.getUShort("brightness", 128);
   brightness_prefs.end();
   return brightness;
 }
